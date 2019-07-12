7855469e74b2837e9d156d5e9c98ae57562aff80
baron <baron@qq.com>
2019-07-12 15:21:45 +0800

    配置数据库

--- Blog/settings.py
+++ Blog/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'Blog_5_22.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'database_name',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
     }
 }
 
